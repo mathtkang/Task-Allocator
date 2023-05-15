5f60c7d50c2d895e6f972bd333840ead895ec509
sgaeng <ksge1124@gmail.com>
2023-05-16 05:47:00 +0900

    for deploy

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,6 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     # "rest_framework.authtoken",
     # "corsheaders",
-    "drf_yasg",
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS + THIRD_PARTY_APPS
